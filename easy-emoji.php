f9dea169081bb3613c8c09948fa2f3daf97edc19
eATM <eatm.app@gmail.com>
2023-05-19 01:19:51 +0800

    11

--- easy-emoji.php
+++ easy-emoji.php
@@ -10,29 +10,30 @@
  */
 
 // 添加表情按钮
-function mce_smiley_button($buttons)
+function mce_button($buttons)
 {
-	array_push($buttons, 'smiley'); // 添加 'smiley' 按钮到 TinyMCE 编辑器中
+	array_push($buttons, 'easy_emoji'); // 添加 'smiley' 按钮到 TinyMCE 编辑器中
 	return $buttons;
 }
-add_filter('mce_buttons', 'mce_smiley_button');
+add_filter('mce_buttons', 'mce_button');
 
-// 添加表情插件
-function mce_smiley_js($plugin_array)
+
+
+// 添加js代码
+function mce_javascript($plugin_array)
 {
-	$plugin_array['smiley'] = plugins_url('/plugin.js', __FILE__); // 指定表情插件的 js 文件路径
+	$plugin_array['easy_emoji'] = plugins_url('/plugin.js', __FILE__); // 指定表情插件的 js 文件路径
 	return $plugin_array;
 }
-add_filter('mce_external_plugins', 'mce_smiley_js');
+add_filter('mce_external_plugins', 'mce_javascript');
 
-// 添加表情样式
-function mce_smiley_css()
+// 添加css样式
+function mce_styles()
 {
-	wp_enqueue_style('tinymce-smiley-button', plugins_url('/plugin.css', __FILE__)); // 加载表情插件的 css 样式
+	wp_enqueue_style('tinymce-easy_emoji-button', plugins_url('/plugin.css', __FILE__)); // 加载表情插件的 css 样式
 }
-add_action('admin_enqueue_scripts', 'mce_smiley_css');
-add_action('wp_enqueue_scripts', 'mce_smiley_css');
-
+add_action('admin_enqueue_scripts', 'mce_styles');
+add_action('wp_enqueue_scripts', 'mce_styles');
 
 function getImageJson()
 {
@@ -66,14 +67,46 @@ function getImageJson()
 	return $result;
 }
 
-function mce_smiley_settings($settings)
+function mce_html_init($settings)
 {
-	if (get_option('use_smilies')) { // 如果启用了表情
-		$smilies = getImageJson();
+	$smilies = getImageJson();
+	echo '<script>window.easy_emoji_json_data = ' . json_encode($smilies) . '</script>'; // 将表情设置以 JavaScript 对象的形式传递给客户端
 
-		echo '<script>window.easy_emoji_json_data = ' . json_encode($smilies) . '</script>'; // 将表情设置以 JavaScript 对象的形式传递给客户端
-	}
 	return $settings;
 }
 
-add_filter('tiny_mce_before_init', 'mce_smiley_settings');
+add_filter('tiny_mce_before_init', 'mce_html_init');
+
+
+function easy_emoji_clock_assets()
+{
+	$dependencies = array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-rich-text');
+	$version = "v1.0.0.1";
+
+	// Enqueue scripts.
+	wp_enqueue_script(
+		'easy-emoji-block-script',
+		plugins_url('/plugin.js', __FILE__),
+		$dependencies,
+		$version,
+		true
+	);
+
+	// Load script translations: https://developer.wordpress.org/reference/functions/wp_set_script_translations/
+	//wp_set_script_translations('easy-emoji-block-script', 'emoji-toolbar', plugin_dir_path(__FILE__) . '/languages/');
+
+	// Enqueue styles.
+	wp_enqueue_style(
+		'easy-emoji-block-script-style',
+		plugins_url('/plugin.css', __FILE__),
+		array(),
+		$version
+	);
+
+	wp_localize_script('easy-emoji-json-data', 'my_data', array(
+		'foo' => 'bar',
+		'baz' => 'qux'
+	));
+}
+
+add_action('enqueue_block_editor_assets', 'easy_emoji_clock_assets');
--- plugin.js
+++ plugin.js
@@ -1,125 +1,172 @@
 /* global tinymce, twemoji, _wpemojiSettings, _smileySettings */
 
+
 (function () {
-    function getHtml() {
 
-        let json_data = window.easy_emoji_json_data;
-        
-        window.easy_emoji_switch = function (elementID, listName, n) {
-            var elem = document.getElementById(elementID);
-            var elemlist = elem.getElementsByTagName("li");
-            for (var i = 0; i < elemlist.length; i++) {
-                elemlist[i].className = "normal";
-                var m = i + 1;
-                document.getElementById(listName + "_" + m).className = "normal";
-            }
-            elemlist[n - 1].className = "current";
-            document.getElementById(listName + "_" + n).className = "current";
-        }
+    //经典编辑器
+    wp.domReady(function () {
+        function getHtml() {
 
-        let tabBarString = "";
-        {
-            let tagNameStr = "";
-            {
-                let index = 1;
-                for (key in json_data) {
-                    if (index == 1) {
-                        tagNameStr += `        <li  class="current" onmouseover="easy_emoji_switch('hotnews_caption','list',${index++});">${key}</li>`;
-                    } else {
-                        tagNameStr += `        <li  class="normal" onmouseover="easy_emoji_switch('hotnews_caption','list',${index++});">${key}</li>`;
-                    }
-                    tagNameStr += "\n"
+            let json_data = window.easy_emoji_json_data;
+
+            window.easy_emoji_switch = function (elementID, listName, n) {
+                var elem = document.getElementById(elementID);
+                var elemlist = elem.getElementsByTagName("li");
+                for (var i = 0; i < elemlist.length; i++) {
+                    elemlist[i].className = "normal";
+                    var m = i + 1;
+                    document.getElementById(listName + "_" + m).className = "normal";
                 }
+                elemlist[n - 1].className = "current";
+                document.getElementById(listName + "_" + n).className = "current";
             }
-            tabBarString = `
+
+            let tabBarString = "";
+            {
+                let tagNameStr = "";
+                {
+                    let index = 1;
+                    for (key in json_data) {
+                        if (index == 1) {
+                            tagNameStr += `        <li  class="current" onmouseover="easy_emoji_switch('hotnews_caption','list',${index++});">${key}</li>`;
+                        } else {
+                            tagNameStr += `        <li  class="normal" onmouseover="easy_emoji_switch('hotnews_caption','list',${index++});">${key}</li>`;
+                        }
+                        tagNameStr += "\n"
+                    }
+                }
+                tabBarString = `
                 <div id="hotnews_caption">
                     <ul style="margin-left:0px;">
                 ${tagNameStr}
                     </ul>
                 </div>
             `
-        }
-
-        let tabContent = '<div id="hotnews_content">\n'
-        {
-            let tabContentItems = "";
-            let index = 1;
-            for (key in json_data) {
-                let beginStr = "";
-                if (index == 1) {
-                    beginStr = `<div class="current" id="list_${index++}">`
-                } else {
-                    beginStr = `<div class="normal" id="list_${index++}">`
-                }
-                beginStr += "\n";
-
-                let item = json_data[key]
-
-                for (imgname in item.names) {
-                    //let imgStr = `<div class="IMG_PARENT"> <img src="${item.url + item.names[imgname]}"> </div>`
-                    let imgStr = `<img src="${item.url + item.names[imgname]}" >`
-                    beginStr = beginStr + "\t" + imgStr + "\n"
-                }
-
-                beginStr += "</div>";
-
-                tabContentItems += beginStr
-                tabContentItems += "\n"
             }
-            tabContent += tabContentItems;
-            tabContent += '\n'
-        }
 
-        let curHtml = `
+            let tabContent = '<div id="hotnews_content">\n'
+            {
+                let tabContentItems = "";
+                let index = 1;
+                for (key in json_data) {
+                    let beginStr = "";
+                    if (index == 1) {
+                        beginStr = `<div class="current" id="list_${index++}">`
+                    } else {
+                        beginStr = `<div class="normal" id="list_${index++}">`
+                    }
+                    beginStr += "\n";
+
+                    let item = json_data[key]
+
+                    for (imgname in item.names) {
+                        //let imgStr = `<div class="IMG_PARENT"> <img src="${item.url + item.names[imgname]}"> </div>`
+                        let imgStr = `<img src="${item.url + item.names[imgname]}" >`
+                        beginStr = beginStr + "\t" + imgStr + "\n"
+                    }
+
+                    beginStr += "</div>";
+
+                    tabContentItems += beginStr
+                    tabContentItems += "\n"
+                }
+                tabContent += tabContentItems;
+                tabContent += '\n'
+            }
+
+            let curHtml = `
         <div id="easy_emoji">
             ${tabBarString}
             ${tabContent}
         </div>
         `
 
-        return curHtml;
-    }
+            return curHtml;
+        }
+        tinymce.PluginManager.add('easy_emoji', function (editor) {
+            editor.addButton('easy_emoji', {
+                type: 'panelbutton',
+                panel: {
+                    classes: 'easy-panel',
+                    role: 'application',
+                    autohide: true,
+                    html: getHtml,
+                    onShow: function (e) {
+                        let id = this._id;
 
-    tinymce.PluginManager.add('smiley', function (editor) {
-        editor.addButton('smiley', {
-            type: 'panelbutton',
-            panel: {
-                classes: 'easy-panel',
-                role: 'application',
-                autohide: true,
-                html: getHtml,
-                onShow: function (e) {
-                    let id = this._id;
+                        //需要删除这个面板高度
+                        let resetHeigth;
+                        resetHeigth = function () {
+                            let ok = false;
+                            do {
+                                let ele = document.getElementById(id)
+                                if (!ele) break;
+                                if (ele.style.height == "") break;
+                                ele.style.height = ""
+                                ele.style.width = ""
+                                ok = true;
+                            } while (false)
 
-                    //需要删除这个面板高度
-                    let resetHeigth;
-                    resetHeigth = function () {
-                        let ok = false;
-                        do {
-                            let ele = document.getElementById(id)
-                            if (!ele) break;
-                            if (ele.style.height == "") break;
-                            ele.style.height = ""
-                            ele.style.width = ""
-                            ok = true;
-                        } while (false)
+                            if (!ok) setTimeout(resetHeigth, 10);
+                        }
+                        resetHeigth();
+                    },
+                    onclick: function (e) {
+                        var img = e.target;
+                        if (img && img.tagName == "IMG") {
+                            var imgHtml = "<img width=48 height=48 src='" + img.currentSrc + "'>"
+                            editor.insertContent(imgHtml);
+                        }
 
-                        if (!ok) setTimeout(resetHeigth, 10);
+                        this.hide();
+                        //debugger;
                     }
-                    resetHeigth();
                 },
-                onclick: function (e) {
-                    var img = e.target;
-                    if (img && img.tagName == "IMG") {
-                        var imgHtml = "<img width=48 height=48 src='" + img.currentSrc + "'>"
-                        editor.insertContent(imgHtml);
-                    }
-
-                    this.hide();
-                    //debugger;
-                }
-            },
-            tooltip: 'Emoticons'
+                tooltip: 'Emoticons'
+            });
         });
     });
+
+
+    //新版块编辑器
+    let plugin_name = "easy/emoji";
+
+    const { registerFormatType, unregisterFormatType, toggleFormat, applyFormat } = window.wp.richText;
+    const { BlockControls } = wp.blockEditor;
+    const { ToolbarGroup, ToolbarButton } = wp.components;
+    const { registerBlockType } = wp.blocks;
+
+    unregisterFormatType(plugin_name);
+
+    const buttonEasyEmoji = function ({ isActive, onChange, value }) {
+        const onClick = function () {
+            alert("还没写完...")
+            debugger;
+            onChange(
+                wp.richText.insert(value, "x")
+            );
+        };
+        return wp.element.createElement(
+            BlockControls,
+            null,
+            wp.element.createElement(
+                ToolbarGroup,
+                null,
+                wp.element.createElement(ToolbarButton, {
+                    icon: "easy_emoji",
+                    title: "Insert custom HTML",
+                    onClick: onClick,
+                    isActive: isActive
+                })
+            )
+        );
+    };
+
+    registerFormatType(plugin_name, {
+        title: "Sample output",
+        tagName: "samp",
+        className: null,
+        edit: buttonEasyEmoji
+    });
+
 })();
