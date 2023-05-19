069046851f663225b682912a07d9daebefc5e39b
eATM <eatm.app@gmail.com>
2023-05-19 18:19:09 +0800

    支持块编辑器

--- easy-emoji.php
+++ easy-emoji.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: easy emoji
  * Description: 让TinyMCE支持添加表情图片.
- * Plugin URI: http://wordpress.org/extend/plugins/easy-emoji/
+ * Plugin URL: https://github.com/eAtmApp/easy-emoji/
  * Version: 1.0.1
  * Author: SilenceRet
  * Author URI: https://eatm.app/
@@ -17,32 +17,6 @@ function mce_button($buttons)
 }
 //add_filter('mce_buttons', 'mce_button',999);	//mce_buttons会被Elementor清除掉,所以用下面第2行工具栏
 add_filter('mce_buttons_2', 'mce_button', 999);	//第2行工具栏
-
-/* 
-//不用这个添加css了,Elementor没进入
-function easy_style()
-{
-	wp_enqueue_style('easy-emoji-style', plugins_url('/plugin.css', __FILE__)); // 加载表情插件的 css 样式
-}
-add_action('init', "easy_style"); */
-//add_action('admin_enqueue_scripts', 'easy_style');
-//add_action('wp_enqueue_scripts', 'easy_style');
-
-
-// 添加css样式
-/*
-
-function easy_script()
-{
-	wp_enqueue_script('easy-emoji-script', plugins_url('/plugin.js', __FILE__), array(), '1.0.0.1', true);
-}
-*/
-
-//用作测试.
-//add_action('admin_enqueue_scripts', 'easy_script');
-//add_action('wp_enqueue_scripts', 'easy_script'); 
-
-
 
 function get_image_json_url()
 {
@@ -70,22 +44,11 @@ function get_image_json_url()
 		}
 	}
 
-	$dataStr='window.easy_emoji_json_data = ' . json_encode($result);
-	file_put_contents(plugin_dir_path(__FILE__).'/data.js', $dataStr);
+	$dataStr = 'window.easy_emoji_json_data = ' . json_encode($result);
+	file_put_contents(plugin_dir_path(__FILE__) . '/data.js', $dataStr);
 
 	return plugins_url('/data.js', __FILE__);
 }
-
-/* 
-//这个方式注入不进Elementor
-function mce_html_init($settings)
-{
-	$smilies = getImageJson();
-	echo '<script>window.easy_emoji_json_data = ' . json_encode($smilies) . '</script>'; // 将表情设置以 JavaScript 对象的形式传递给客户端
-	return $settings;
-}
-add_filter('tiny_mce_before_init', 'mce_html_init'); */
-
 
 //让前台的TinyMCE加载时,能加载自己的js代码
 function mce_javascript($plugin_array)
@@ -100,36 +63,35 @@ function mce_javascript($plugin_array)
 }
 add_filter('mce_external_plugins', 'mce_javascript', 999, 1);
 
-
 function easy_emoji_clock_assets()
 {
 	$dependencies = array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-rich-text');
 	$version = "v1.0.0.1";
-	
+
 	// Enqueue scripts.
 	wp_enqueue_script(
-		'easy-emoji-block-script',
+		'easy-emoji-block--data',
 		get_image_json_url(),
 		$dependencies,
 		$version,
 		true
 	);
 
-	// Load script translations: https://developer.wordpress.org/reference/functions/wp_set_script_translations/
-	//wp_set_script_translations('easy-emoji-block-script', 'emoji-toolbar', plugin_dir_path(__FILE__) . '/languages/');
+	wp_enqueue_script(
+		'easy-emoji-block-script',
+		plugins_url('/plugin.js', __FILE__),
+		$dependencies,
+		$version,
+		true
+	);
 
 	// Enqueue styles.
-/* 	wp_enqueue_style(
+	/* 	wp_enqueue_style(
 		'easy-emoji-block-script-style',
 		plugins_url('/plugin.css', __FILE__),
 		array(),
 		$version
 	); */
-
-/* 	wp_localize_script('easy-emoji-json-data', 'my_data', array(
-		'foo' => 'bar',
-		'baz' => 'qux'
-	)); */
 }
 
 add_action('enqueue_block_editor_assets', 'easy_emoji_clock_assets');
--- plugin.js
+++ plugin.js
@@ -1,11 +1,5 @@
 /* global tinymce, twemoji, _wpemojiSettings, _smileySettings */
 
-function isElementor() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const action = urlParams.get('action');
-    return action === 'elementor'
-}
-
 function loadCSS() {
     let url = "/wp-content/plugins/easy-emoji/plugin.css"
     var link = document.createElement('link');
@@ -139,51 +133,97 @@ loadCSS();
         });
     };
 
-    console.log("easy emoji script loaded");
-
     if (typeof (tinymce) != 'undefined') {
         tinymce.PluginManager.add('easy_emoji', easy_emoji_mce_function);
+        return;
     }
-    
-    /*     //新版块编辑器
-        let plugin_name = "easy/emoji";
 
-        const { registerFormatType, unregisterFormatType, toggleFormat, applyFormat } = window.wp.richText;
-        const { BlockControls } = wp.blockEditor;
-        const { ToolbarGroup, ToolbarButton } = wp.components;
-        const { registerBlockType } = wp.blocks;
-    
-        unregisterFormatType(plugin_name);
-    
-        const buttonEasyEmoji = function ({ isActive, onChange, value }) {
-            const onClick = function () {
-                alert("还没写完...")
-                debugger;
-                onChange(
-                    wp.richText.insert(value, "x")
-                );
-            };
-            return wp.element.createElement(
-                BlockControls,
-                null,
-                wp.element.createElement(
-                    ToolbarGroup,
-                    null,
-                    wp.element.createElement(ToolbarButton, {
-                        icon: "easy_emoji",
-                        title: "Insert custom HTML",
-                        onClick: onClick,
-                        isActive: isActive
-                    })
-                )
-            );
+    //新版块编辑器
+    let plugin_name = "easy/emoji";
+
+    const { registerFormatType, unregisterFormatType, toggleFormat, applyFormat } = window.wp.richText;
+    const { BlockControls } = wp.blockEditor;
+    const { ToolbarGroup, ToolbarButton } = wp.components;
+    const { registerBlockType } = wp.blocks;
+
+    unregisterFormatType(plugin_name);
+
+    let showEmojiDialog = function (cb) {
+        let x = 0, y = 0;
+        {
+            let parentDiv = document.querySelector('.dashicons-easy_emoji');
+            while (parentDiv.parentElement && parentDiv.parentElement.tagName !== 'DIV') {
+                parentDiv = parentDiv.parentElement;
+            }
+            let butRect = parentDiv.getBoundingClientRect();
+            x = butRect.x - 10;
+            y = butRect.bottom + 3;
+        }
+
+        let dlg = document.getElementById("easy_emoji_panel");
+        if (!dlg) {
+            let htmlStr = `
+                <div class="easy_popup-content" id="easy_emoji_content">
+                    ${get_div_html()}
+                </div>
+            `;
+            let div = document.createElement("div");
+            div.id = "easy_emoji_panel";
+            div.className = "easy_popup";
+            div.innerHTML = htmlStr;
+            document.body.appendChild(div);
+            dlg = document.getElementById("easy_emoji_panel");
+        }
+
+        let dlgContent = document.getElementById("easy_emoji_content");
+        dlgContent.style.left = x + "px";
+        dlgContent.style.top = y + "px";
+        dlg.style.display = "block";
+
+        dlg.onclick = function (e) {
+            dlg.style.display = "none";
+            if (e.target.tagName != "IMG") return;
+            cb(e.target.src);
+        }
+    }
+
+    const buttonEasyEmoji = function ({ isActive, onChange, value }) {
+        const onClick = function () {
+
+            showEmojiDialog((url) => {
+                obj = wp.richText.create({
+                    html: `<img src="${url}" width="32">`,
+                });
+                onChange(wp.richText.insert(
+                    value,
+                    obj,
+                    value.start,
+                    value.end
+                ));
+            });
         };
-    
-        registerFormatType(plugin_name, {
-            title: "Sample output",
-            tagName: "samp",
-            className: null,
-            edit: buttonEasyEmoji
-        }); */
+
+        return wp.element.createElement(
+            BlockControls,
+            null,
+            wp.element.createElement(
+                ToolbarGroup,
+                null,
+                wp.element.createElement(ToolbarButton, {
+                    icon: "easy_emoji",
+                    title: "插入表情图片",
+                    onClick: onClick,
+                    isActive: isActive
+                })
+            )
+        );
+    };
+
+    registerFormatType(plugin_name, {
+        title: "easy_emoji",
+        tagName: "easy_emoji",
+        className: null,
+        edit: buttonEasyEmoji
+    });
 
 })();
